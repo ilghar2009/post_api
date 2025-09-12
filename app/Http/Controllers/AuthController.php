df2a768b4ccffe826d71c13f12339f3b174d3186
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-09-12 13:19:50 +0330

    create login and token model,migration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\UserToken;
 use Carbon\Carbon;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\Password;
 
@@ -23,10 +25,35 @@ class AuthController extends Controller
         $user->password = $request->password;
         $user->save();
 
-        return true;
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user
+        ],201);
+
     }
 
-    public function login(){
+    public function login(Request $request){
+        $check_user = $request->only('username', 'password');
+
+        if(!$check_user || !Auth::attempt($check_user)){
+            return response()->json(['error' => 'invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = Str::uuid();
+
+        UserToken::create([
+           'user_id' => $user->user_id,
+           'access_token' => $token,
+           'expires' => Carbon::now()->addMinute(30),
+        ]);
+
+        return response()->json([
+            'message' => 'login successful',
+            'token_access' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
 
     }
 }
